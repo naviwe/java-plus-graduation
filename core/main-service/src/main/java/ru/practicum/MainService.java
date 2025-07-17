aed0a0ad5b4ecf6b564bad60fa6c78ade5e45633
naviwe <timurnovotorzhin@yandex.kz>
2025-07-17 15:14:26 +0400

    feat: refactoring to spring-cloud

--- core/main-service/src/main/java/ru/practicum/MainService.java
+++ core/main-service/src/main/java/ru/practicum/MainService.java
@@ -2,14 +2,14 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-
+@EnableFeignClients(basePackages = "ru.practicum")
 @SpringBootApplication
-@EnableDiscoveryClient
-public class StatsServer {
-    public static void main(String[] args) {
-        SpringApplication.run(StatsServer.class,args);
+public class MainService {
 
+    public static void main(String[] args) {
+
+        SpringApplication.run(MainService.class,args);
     }
 }
--- infra/gateway-server/src/main/java/ru/practicum/Gateway.java
+++ infra/gateway-server/src/main/java/ru/practicum/Gateway.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
 @SpringBootApplication
 @EnableDiscoveryClient
-public class StatsServer {
+public class Gateway {
     public static void main(String[] args) {
-        SpringApplication.run(StatsServer.class,args);
-
+        SpringApplication.run(Gateway.class, args);
     }
 }
