1470eb0fe478f2621a2a1260877faca58a76a971
naviwe <timurnovotorzhin@yandex.kz>
2025-07-19 14:33:38 +0400

    feat: refactoring to spring-cloud

--- core/main-service/src/main/java/ru/practicum/MainService.java
+++ core/main-service/src/main/java/ru/practicum/MainService.java
@@ -2,8 +2,12 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class MainService {
 
     public static void main(String[] args) {
