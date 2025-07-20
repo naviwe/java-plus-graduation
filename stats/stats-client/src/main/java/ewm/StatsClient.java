cc0beb7bd29e6da3982972513c8bed82e483740d
naviwe <timurnovotorzhin@yandex.kz>
2025-07-20 19:05:44 +0400

    fix:code

--- stats/stats-client/src/main/java/ewm/StatsClient.java
+++ stats/stats-client/src/main/java/ewm/StatsClient.java
@@ -1,4 +1,4 @@
-package ewm.src.main.java.ru.practicum;
+package ewm;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import ewm.EndpointHitDto;
-import ewm.StatsDto;
 
 import java.util.List;
 
