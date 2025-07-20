6aa715474e079b38774f6ea151f784976ff2d1eb
naviwe <timurnovotorzhin@yandex.kz>
2025-07-20 16:07:23 +0400

    fix: code

--- stats/stats-server/src/main/java/ru/practicum/StatsServer.java
+++ stats/stats-server/src/main/java/ru/practicum/StatsServer.java
@@ -2,12 +2,8 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@ConfigurationPropertiesScan
 public class StatsServer {
     public static void main(String[] args) {
         SpringApplication.run(StatsServer.class,args);
