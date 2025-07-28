98b77c4793d1683135d60e60f41bf846a47dafe5
naviwe <timurnovotorzhin@yandex.kz>
2025-07-28 23:41:35 +0400

    fix: event

--- core/interaction-api/src/main/java/ewm/interaction/client/EventFeignClient.java
+++ core/interaction-api/src/main/java/ewm/interaction/client/EventFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "event-service")
+@FeignClient(name = "event-service", path = "/events/feign")
 public interface EventFeignClient {
 
     @GetMapping("/events/{eventId}")
