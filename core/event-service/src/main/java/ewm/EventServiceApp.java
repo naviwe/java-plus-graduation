ce816a3a028a0888c79c13a74c047d60c6528982
naviwe <timurnovotorzhin@yandex.kz>
2025-07-30 00:47:32 +0400

    fix: Feign

--- core/event-service/src/main/java/ewm/EventServiceApp.java
+++ core/event-service/src/main/java/ewm/EventServiceApp.java
@@ -2,14 +2,17 @@ package ewm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
-@EnableDiscoveryClient
-@EnableFeignClients
+@SpringBootApplication(scanBasePackages = {
+        "ewm",
+        "ewm.interaction"
+})
+@EnableFeignClients(basePackages = {"ewm.interaction.client","ewm.src.main.java"})
 public class EventServiceApp {
+
     public static void main(String[] args) {
-        SpringApplication.run(EventServiceApp.class, args);
+
+        SpringApplication.run(EventServiceApp.class,args);
     }
 }
--- core/request-service/src/main/java/ewm/RequestServiceApp.java
+++ core/request-service/src/main/java/ewm/RequestServiceApp.java
@@ -6,10 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableFeignClients
+@EnableFeignClients(basePackages = {"ewm.interaction.client","ewm.src.main.java"})
 public class RequestServiceApp {
     public static void main(String[] args) {
-        SpringApplication.run(RequestServiceApp.class, args);
+        SpringApplication.run(RequestServiceApp.class,args);
     }
 }
