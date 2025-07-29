5be536117b83bb69d0038a90e9107b88a70f07d5
naviwe <timurnovotorzhin@yandex.kz>
2025-07-30 00:50:59 +0400

    fix: Feign

--- core/request-service/src/main/java/ewm/RequestServiceApp.java
+++ core/request-service/src/main/java/ewm/RequestServiceApp.java
@@ -2,11 +2,10 @@ package ewm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = {"ewm.interaction.client","ewm.src.main.java"})
+@EnableFeignClients(basePackages = {"ewm.client", "ewm.interaction.client"})
 public class RequestServiceApp {
     public static void main(String[] args) {
         SpringApplication.run(RequestServiceApp.class,args);
